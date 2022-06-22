7388d46ffe14554ed46b49c1b6b271ceadbd4dd4
Marcio Souza <marciosouzaunico@gmail.com>
2022-06-22 17:09:11 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# rocketmovies-api
+ 
---
+++ src/routes/tags.routes.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+
+const TagsController = require("../controllers/TagsController");
+
+const tagsRoutes = Router();
+
+const tagsController = new TagsController();
+
+
+tagsRoutes.get("/", tagsController.index);
+
+
+module.exports = tagsRoutes;
